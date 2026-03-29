d94df2ac8b22a53a14f571e1ed4419a2051c7315
刘剑锋 <3169261077@qq.com>
2026-03-29 08:59:09 +0800

    style: 样式修复

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,41 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import viteImagemin from 'vite-plugin-imagemin'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/order/',
-  plugins: [vue()],
-  server: {
-    hmr: {
-      clientPort: 5173,
-      protocol: 'ws',
-      path: '/hmr/'
+  plugins: [
+      vue(),
+      // 图片压缩
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 20,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+      }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     }
-  }
+  },
+  server: {
+    port: 5174,
+    hmr: {
+      clientPort: 5174,
+      protocol: 'ws',
+      host: 'localhost',
+    }
+  },
 })
