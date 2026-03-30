1310f18ae97d04541a64296a30381b7ae949fd15
刘剑锋 <3169261077@qq.com>
2026-03-30 20:17:45 +0800

    style: 添加修改密码

--- src/components/ForgotPasswordDrawer.vue
+++ src/components/ForgotPasswordDrawer.vue
@@ -10,115 +10,122 @@
   >
     <div class="drawer-content">
       <!-- 标题 -->
-      <div class="forgot-title">忘记密码</div>
+      <div class="forgot-title">{{ currentStep === 1 ? '忘记密码' : '设置新密码' }}</div>
       
-      <!-- 手机号输入 -->
-      <div class="input-group">
-        <div class="phone-input-wrapper">
-          <img src="../assets/icons/Smatphone.svg" class="input-icon phone-left-icon" />
-          <el-dropdown trigger="click" @command="handleCountryChange" class="country-dropdown">
-            <div class="country-code">
-              <span class="code">{{ selectedCountry.code }}</span>
-              <el-icon class="arrow-icon"><ArrowDown /></el-icon>
+      <!-- 第一步：手机号和验证码 -->
+      <template v-if="currentStep === 1">
+        <!-- 手机号输入 -->
+        <div class="input-group">
+          <div class="phone-input-wrapper">
+            <img src="../assets/icons/Smatphone.svg" class="input-icon phone-left-icon" />
+            <el-dropdown trigger="click" @command="handleCountryChange" class="country-dropdown">
+              <div class="country-code">
+                <span class="code">{{ selectedCountry.code }}</span>
+                <el-icon class="arrow-icon"><ArrowDown /></el-icon>
+              </div>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item 
+                    v-for="country in countryCodes" 
+                    :key="country.code" 
+                    :command="country"
+                    class="country-dropdown-item"
+                  >
+                    <img :src="country.icon" class="dropdown-flag-icon" />
+                    <span class="dropdown-country-name">{{ country.name }}</span>
+                    <span class="dropdown-country-code">{{ country.code }}</span>
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <el-input
+              v-model="form.phone"
+              placeholder="请输入手机号"
+              class="phone-input"
+            />
+            <img :src="selectedCountry.icon" class="input-icon flag-icon" />
+          </div>
+        </div>
+        
+        <!-- 验证码输入 -->
+        <div class="input-group">
+          <div class="code-input-wrapper">
+            <img src="../assets/icons/yzm.svg" class="input-icon code-left-icon" />
+            <el-input
+              v-model="form.code"
+              placeholder="请输入验证码"
+              class="code-input"
+              maxlength="6"
+            />
+            <span
+              class="send-code-btn"
+              :class="{ disabled: countdown > 0 || !isPhoneValid }"
+              @click="sendCode"
+            >
+              {{ countdown > 0 ? `${countdown}s` : '获取验证码' }}
+            </span>
+          </div>
+        </div>
+      </template>
+      
+      <!-- 第二步：设置新密码 -->
+      <template v-if="currentStep === 2">
+        <!-- 新密码输入 -->
+        <div class="input-group">
+          <div class="password-input-wrapper">
+            <img src="../assets/icons/mima.svg" class="input-icon password-left-icon" />
+            <el-input
+              v-model="form.newPassword"
+              :type="showNewPassword ? 'text' : 'password'"
+              placeholder="请输入新密码"
+              class="password-input"
+            />
+            <div class="eye-icon-wrapper" @click="showNewPassword = !showNewPassword">
+              <img v-if="showNewPassword" src="../assets/icons/yy.svg" class="eye-icon-img" />
+              <img v-else src="../assets/icons/yn.svg" class="eye-icon-img" />
             </div>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item 
-                  v-for="country in countryCodes" 
-                  :key="country.code" 
-                  :command="country"
-                  class="country-dropdown-item"
-                >
-                  <img :src="country.icon" class="dropdown-flag-icon" />
-                  <span class="dropdown-country-name">{{ country.name }}</span>
-                  <span class="dropdown-country-code">{{ country.code }}</span>
-                </el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-          <el-input
-            v-model="form.phone"
-            placeholder="请输入手机号"
-            class="phone-input"
-          />
-          <img :src="selectedCountry.icon" class="input-icon flag-icon" />
-        </div>
-      </div>
-      
-      <!-- 验证码输入 -->
-      <div class="input-group">
-        <div class="code-input-wrapper">
-          <img src="../assets/icons/yzm.svg" class="input-icon code-left-icon" />
-          <el-input
-            v-model="form.code"
-            placeholder="请输入验证码"
-            class="code-input"
-            maxlength="6"
-          />
-          <span
-            class="send-code-btn"
-            :class="{ disabled: countdown > 0 || !isPhoneValid }"
-            @click="sendCode"
-          >
-            {{ countdown > 0 ? `${countdown}s` : '获取验证码' }}
-          </span>
-        </div>
-      </div>
-      
-      <!-- 新密码输入 -->
-      <div class="input-group">
-        <div class="password-input-wrapper">
-          <img src="../assets/icons/mima.svg" class="input-icon password-left-icon" />
-          <el-input
-            v-model="form.newPassword"
-            :type="showNewPassword ? 'text' : 'password'"
-            placeholder="请输入新密码"
-            class="password-input"
-          />
-          <div class="eye-icon-wrapper" @click="showNewPassword = !showNewPassword">
-            <img v-if="showNewPassword" src="../assets/icons/yy.svg" class="eye-icon-img" />
-            <img v-else src="../assets/icons/yn.svg" class="eye-icon-img" />
           </div>
         </div>
-      </div>
-      
-      <!-- 确认新密码输入 -->
-      <div class="input-group">
-        <div class="password-input-wrapper">
-          <img src="../assets/icons/mima.svg" class="input-icon password-left-icon" />
-          <el-input
-            v-model="form.confirmPassword"
-            :type="showConfirmPassword ? 'text' : 'password'"
-            placeholder="请再次输入新密码"
-            class="password-input"
-          />
-          <div class="eye-icon-wrapper" @click="showConfirmPassword = !showConfirmPassword">
-            <img v-if="showConfirmPassword" src="../assets/icons/yy.svg" class="eye-icon-img" />
-            <img v-else src="../assets/icons/yn.svg" class="eye-icon-img" />
+        
+        <!-- 确认新密码输入 -->
+        <div class="input-group">
+          <div class="password-input-wrapper">
+            <img src="../assets/icons/mima.svg" class="input-icon password-left-icon" />
+            <el-input
+              v-model="form.confirmPassword"
+              :type="showConfirmPassword ? 'text' : 'password'"
+              placeholder="请再次输入新密码"
+              class="password-input"
+            />
+            <div class="eye-icon-wrapper" @click="showConfirmPassword = !showConfirmPassword">
+              <img v-if="showConfirmPassword" src="../assets/icons/yy.svg" class="eye-icon-img" />
+              <img v-else src="../assets/icons/yn.svg" class="eye-icon-img" />
+            </div>
           </div>
         </div>
-      </div>
+        
+        <!-- 密码提示 -->
+        <div class="password-tips">
+          <p>密码应由字母加数字组成，首字母必须大写，长度最少8位</p>
+          <p>请妥善保管密码，并避免使用简单密码组合</p>
+        </div>
+      </template>
       
-      <!-- 密码提示 -->
-      <div class="password-tips">
-        <p>密码应由字母加数字组成，首字母必须大写，长度最少8位</p>
-        <p>请妥善保管密码，并避免使用简单密码组合</p>
-      </div>
-      
-      <!-- 确定按钮 -->
+      <!-- 下一步/确定按钮 -->
       <el-button
         type="primary"
         size="large"
         class="submit-btn"
+        :class="{ 'confirm-btn': currentStep === 2 }"
         @click="handleSubmit"
         :loading="loading"
         :disabled="!canSubmit"
       >
-        确定
+        {{ currentStep === 1 ? '下一步' : '确定' }}
       </el-button>
       
       <!-- 切换登录 -->
-      <div class="switch-login">
+      <div v-if="currentStep === 1" class="switch-login">
         <a href="#" class="switch-login-btn" @click.prevent="switchToLogin">切换登录</a>
       </div>
     </div>
@@ -142,6 +149,7 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue', 'reset-success', 'switch-login'])
 
 const drawerVisible = ref(props.modelValue)
+const currentStep = ref(1)
 const countdown = ref(0)
 const showNewPassword = ref(false)
 const showConfirmPassword = ref(false)
@@ -186,10 +194,11 @@ const isPasswordValid = computed(() => {
 
 // 是否可以提交
 const canSubmit = computed(() => {
-  return isPhoneValid.value && 
-         form.value.code.length >= 4 &&
-         isPasswordValid.value && 
-         form.value.newPassword === form.value.confirmPassword
+  if (currentStep.value === 1) {
+    return isPhoneValid.value && form.value.code.length >= 4
+  } else {
+    return isPasswordValid.value && form.value.newPassword === form.value.confirmPassword
+  }
 })
 
 // 处理国家选择
@@ -241,48 +250,55 @@ const startCountdown = () => {
 
 // 处理提交
 const handleSubmit = async () => {
-  if (!isPhoneValid.value) {
-    ElMessage.error('请输入正确的手机号')
-    return
-  }
-  
-  if (form.value.code.length < 4) {
-    ElMessage.error('请输入正确的验证码')
-    return
-  }
-  
-  if (!isPasswordValid.value) {
-    ElMessage.error('密码格式不正确')
-    return
-  }
-  
-  if (form.value.newPassword !== form.value.confirmPassword) {
-    ElMessage.error('两次输入的密码不一致')
-    return
-  }
-  
-  loading.value = true
-  try {
-    const formData = new FormData()
-    formData.append('telephone', form.value.phone)
-    formData.append('phoneCode', selectedCountry.value.code)
-    formData.append('authCode', form.value.code)
-    formData.append('newPassword', form.value.newPassword)
-
-    const response = await forgotPassword(formData)
-    
-    if (response.code === 200) {
-      ElMessage.success('密码重置成功')
-      emit('reset-success')
-      closeDrawer()
-    } else {
-      ElMessage.error(response.msg || '密码重置失败')
+  if (currentStep.value === 1) {
+    // 第一步：验证手机号和验证码
+    if (!isPhoneValid.value) {
+      ElMessage.error('请输入正确的手机号')
+      return
     }
-  } catch (error) {
-    console.error('密码重置错误:', error)
-    ElMessage.error(error.message || '密码重置失败，请稍后重试')
-  } finally {
-    loading.value = false
+    
+    if (form.value.code.length < 4) {
+      ElMessage.error('请输入正确的验证码')
+      return
+    }
+    
+    // 进入第二步
+    currentStep.value = 2
+  } else {
+    // 第二步：提交重置密码
+    if (!isPasswordValid.value) {
+      ElMessage.error('密码格式不正确')
+      return
+    }
+    
+    if (form.value.newPassword !== form.value.confirmPassword) {
+      ElMessage.error('两次输入的密码不一致')
+      return
+    }
+    
+    loading.value = true
+    try {
+      const formData = new FormData()
+      formData.append('telephone', form.value.phone)
+      formData.append('phoneCode', selectedCountry.value.code)
+      formData.append('authCode', form.value.code)
+      formData.append('newPassword', form.value.newPassword)
+
+      const response = await forgotPassword(formData)
+      
+      if (response.code === 200) {
+        ElMessage.success('密码重置成功')
+        emit('reset-success')
+        closeDrawer()
+      } else {
+        ElMessage.error(response.msg || '密码重置失败')
+      }
+    } catch (error) {
+      console.error('密码重置错误:', error)
+      ElMessage.error(error.message || '密码重置失败，请稍后重试')
+    } finally {
+      loading.value = false
+    }
   }
 }
 
@@ -300,6 +316,7 @@ const switchToLogin = () => {
 
 // 重置表单
 const resetForm = () => {
+  currentStep.value = 1
   form.value = {
     phone: '',
     code: '',
@@ -587,6 +604,10 @@ onUnmounted(() => {
   opacity: 0.9;
 }
 
+.confirm-btn {
+  margin-top: 32px;
+}
+
 .switch-login {
   text-align: right;
   margin-top: 16px;
@@ -600,11 +621,6 @@ onUnmounted(() => {
 
 /* 响应式设计 */
 @media (min-width: 768px) {
-  .back-btn {
-    top: 40px;
-    left: 32px;
-  }
-  
   .forgot-title {
     font-size: 22px;
     margin-bottom: 48px;
